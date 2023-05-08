2d0d51b3ae2cbc6ce3a206559f58fe259f9c6de7
Marcos <marcos.macedo@digital2b.com.br>
2023-05-08 15:14:46 -0300

    app.js resolve confilct

--- themes/tailpress/resources/js/app.js
+++ themes/tailpress/resources/js/app.js
@@ -1,12 +1,11 @@
-// Navigation toggle
-<<<<<<< HEAD
-// window.addEventListener('load', function () {
-//       let main_navigation = document.querySelector('#primary-menu');
-//       document.querySelector('#primary-menu-toggle').addEventListener('click', function (e) {
-//             e.preventDefault();
-//             main_navigation.classList.toggle('hidden');
-//       });
-// });
+Navigation toggle
+window.addEventListener('load', function () {
+      let main_navigation = document.querySelector('#primary-menu');
+      document.querySelector('#primary-menu-toggle').addEventListener('click', function (e) {
+            e.preventDefault();
+            main_navigation.classList.toggle('hidden');
+      });
+});
 jQuery(document).ready(function($) {
       $('#modal').on('click', function() {
           $(this).addClass('hidden');
@@ -24,7 +23,6 @@ jQuery(document).ready(function($) {
   
   
   
-=======
 window.addEventListener('DOMContentLoaded', function () {
       let main_navigation = document.querySelector('#primaryNavContent');
       document.querySelector('#hamburguerButton').addEventListener('click', function (e) {
@@ -40,4 +38,3 @@ window.addEventListener('DOMContentLoaded', function () {
             main_navigation.classList.toggle('hidden');
       });
 });
->>>>>>> 6c3972be2a6740375c69be870c6838e349a13280
