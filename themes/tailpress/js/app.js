0896278c0eb89349e4cfaa9f77d0fb1827d302c9
Marcos <marcos.macedo@digital2b.com.br>
2023-05-08 19:04:44 -0300

    Método growp

--- themes/tailpress/js/app.js
+++ themes/tailpress/js/app.js
@@ -7,35 +7,46 @@
   \*****************************/
 /***/ (() => {
 
-// Navigation toggle
-window.addEventListener('load', function () {
-  var main_navigation = document.querySelector('#primary-menu');
-  document.querySelector('#primary-menu-toggle').addEventListener('click', function (e) {
-    e.preventDefault();
-    main_navigation.classList.toggle('hidden');
-  });
-});
 jQuery(document).ready(function ($) {
   $('#modal').on('click', function () {
     $(this).addClass('hidden');
-    $('#player').attr('src', '');
+    $('#videoDep').attr('src', '');
   });
   $('.open-video').on('click', function () {
-    $('#modal').removeClass('hidden');
+    var depID = $(this).attr('depID');
+    console.log('depID: ' + depID);
+    var modalContents = $(".deps-card").find(".video-url");
+    var numModalContents = modalContents.length;
+    for (var i = 0; i < numModalContents; i++) {
+      var modalPostID = modalContents.eq(i).attr("data-id");
+      console.log('modalPostID: ' + modalPostID);
+      console.log(modalContents === modalPostID);
+      if (modalPostID === depID) {
+        var url = $('.video-url[data-id="' + modalPostID + '"]').val();
+        $('#videoDep').attr('src', url + '?autoplay=1');
+        $('#modal').removeClass('hidden');
+        console.log('to no if');
+      }
+    }
   });
-});
-window.addEventListener('DOMContentLoaded', function () {
-  var main_navigation = document.querySelector('#primaryNavContent');
-  document.querySelector('#hamburguerButton').addEventListener('click', function (e) {
-    e.preventDefault();
-    main_navigation.classList.toggle('hidden');
+  var swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
   });
-});
-window.addEventListener('DOMContentLoaded', function () {
-  var main_navigation = document.querySelector('#secondaryNavContent');
-  document.querySelector('#secondaryHamburguerButton').addEventListener('click', function (e) {
-    e.preventDefault();
-    main_navigation.classList.toggle('hidden');
+  $('button').click(function () {
+    $(this).next("p").toggleClass('hidden');
+    if ($(this).next("p").hasClass('hidden')) {
+      $(this).find('svg').attr('class', 'w-6 h-6');
+    } else {
+      $(this).find('svg').attr('class', 'w-6 h-6 transform rotate-45');
+      $(this).next("p").removeClass('opacity-0').addClass('opacity-100').fadeIn();
+    }
   });
 });
 
